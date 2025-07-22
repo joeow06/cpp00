7423db205ec3495f59936c5b46a94fa50d9f06ab
joeow06 <joeow06@gmail.com>
2025-07-22 13:42:44 +0800

    added megaphone.cpp

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 21:19:18 by joeow             #+#    #+#             */
+/*   Updated: 2025/07/21 21:22:04 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av) 
+{
+    int j;
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i  = 1; i < ac; i++)
+        {
+            j = 0;
+            while (av[i][j])
+                std::cout << (char)std::toupper(av[i][j++]);
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
